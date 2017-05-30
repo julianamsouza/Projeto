61a1fe4f057f55887d1e9804e68ee1ab2474d3a7
julianamsouza <souza3408@gmail.com>
2017-05-30 00:04:07 -0300

    Add files via upload

---
+++ Categoria.java
@@ -0,0 +1,33 @@
+package com.dell.juliana.filme.model;
+
+/**
+ * Created by Juliana on 27/05/2017.
+ */
+
+public class Categoria {
+    private String url;
+    private String name;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
+
+
