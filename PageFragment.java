bcc1f6764839d7724b95a2f0978b538e39e40644
julianamsouza <souza3408@gmail.com>
2017-05-30 22:58:38 -0300

    Add files via upload

--- PageFragment.java
+++ PageFragment.java
@@ -11,32 +11,32 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.dell.juliana.filme.NetflixDetailsActivity;
+import com.dell.juliana.filme.CategoriaDetailsActivity;
 import com.dell.juliana.filme.R;
-import com.dell.juliana.filme.model.Netflix;
+import com.dell.juliana.filme.model.Categoria;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PageFragment extends Fragment {
-    public List<Netflix> netflixs = new ArrayList<Netflix>();
-    public ArrayAdapter<Netflix> arrayAdapter;
+    public List<Categoria> categorias = new ArrayList<Categoria>();
+    public ArrayAdapter<Categoria> arrayAdapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_page,container,false);
 
-        ListView lvNetflixs = (ListView) view.findViewById(R.id.lv_netflixs);
+        ListView lvCategorias = (ListView) view.findViewById(R.id.lv_categorias);
 
-        arrayAdapter = new ArrayAdapter<Netflix>(getActivity(),android.R.layout.simple_list_item_1,netflixs);
-        lvNetflixs.setAdapter(arrayAdapter);
+        arrayAdapter = new ArrayAdapter<Categoria>(getActivity(),android.R.layout.simple_list_item_1, categorias);
+        lvCategorias.setAdapter(arrayAdapter);
 
-        lvNetflixs
+        lvCategorias
                 .setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent it = new Intent(getActivity(), NetflixDetailsActivity.class);
+                Intent it = new Intent(getActivity(), CategoriaDetailsActivity.class);
                 startActivity(it);
             }
         });
